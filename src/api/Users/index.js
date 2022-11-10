1ff41f335918f65e0ca12f816835fbcd2a42c335
niragui <ignacioiragu2@gmail.com>
2022-11-09 22:56:06 -0300

    Arreglos?
    Co-authored-by: Mateo Capon Blanquer <mateocapon@users.noreply.github.com>

--- src/api/Users/index.js
+++ src/api/Users/index.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 const GATEWAY_URL = "https://fiuber-gateway.herokuapp.com"
 
+const token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImQ3YjE5MTI0MGZjZmYzMDdkYzQ3NTg1OWEyYmUzNzgzZGMxYWY4OWYiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vZml1YmVyLTM2Yjg2IiwiYXVkIjoiZml1YmVyLTM2Yjg2IiwiYXV0aF90aW1lIjoxNjY4MDQzODk5LCJ1c2VyX2lkIjoiZjdrMnF3dHc5NWhZcmF0bTZkNVZoc1VLTjY1MiIsInN1YiI6ImY3azJxd3R3OTVoWXJhdG02ZDVWaHNVS042NTIiLCJpYXQiOjE2NjgwNDM4OTksImV4cCI6MTY2ODA0NzQ5OSwiZW1haWwiOiJhZG1pbmNvbnRyYXNlbmlhc3RyaW5nQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJhZG1pbmNvbnRyYXNlbmlhc3RyaW5nQGdtYWlsLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIn19.G-e-qfYKa6IYENOprYZnPc8RsjxEu9VfIrWaILLBsCvtcoGeeA3SixZ2Km_CQ33QB-3SChNHtukUjKbIlpl5SUaCdJ6ukpunXROWJ4J_5HE5sFuvzfMT6Jy4BsyhXC1LkVOvv7Qa1m7fDZPFeGwciPqRpr6o78EFueD0StT5nh2DoSaUqfV4_JPBvTVp6PHzPgIKDzDTqjlT-s41NWN5preOMizeL6cxk9Y8F39WeljJ_utT1juW_84rFxEGAj6K4H6vxQ2hCvO50DdxJW9eP6FCLRep2zSt3up_-3lRA9ev7XxT335KBaNBAjU80uGXjqaoBqPqlriRqdfRfSdghw'
+
 export function getUsers() {
   // Aca deberiamos hacer una llamada al gateway
   // Cambiar el nombre de las columnas en modules/Users
@@ -37,7 +39,7 @@ export function getUsers() {
 export function getUserProfile(id, setProfile) {
   axios.get(GATEWAY_URL + "/users/" + id, {
     headers: {
-      'token': 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImQ3YjE5MTI0MGZjZmYzMDdkYzQ3NTg1OWEyYmUzNzgzZGMxYWY4OWYiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vZml1YmVyLTM2Yjg2IiwiYXVkIjoiZml1YmVyLTM2Yjg2IiwiYXV0aF90aW1lIjoxNjY4MDM0ODQzLCJ1c2VyX2lkIjoiZjdrMnF3dHc5NWhZcmF0bTZkNVZoc1VLTjY1MiIsInN1YiI6ImY3azJxd3R3OTVoWXJhdG02ZDVWaHNVS042NTIiLCJpYXQiOjE2NjgwMzQ4NDMsImV4cCI6MTY2ODAzODQ0MywiZW1haWwiOiJhZG1pbmNvbnRyYXNlbmlhc3RyaW5nQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJhZG1pbmNvbnRyYXNlbmlhc3RyaW5nQGdtYWlsLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIn19.On6dcaVudVMcGEOFWga8OHP5_kHIF6_-53NMSWC2ojeAFPrSyMQLiI-vTyD9UuxzhpY_NgA0x6WKtkUDmbHMH4G5NgqYys6GGE8-SaZkbR_8T4Gr_AJxoCDP8fWvzr4cmygo-S1dQSlSCmPB-rZJhWrVGNXQlip1DbGogK6hJS4KvmBeBd7OBo36H5I9wUCak7z6oWUJk79KD1JN6u8exdCNfJv5C0j5_ZLG6H6GBIXnwzU6B0E2ZoeAzBrxXpWoBPSz2iJwRpd52A99v-nvFV36iMPcUamHapxlmBxulo_5Tfni6ho5l9PAMrNnQ6qf0xRlByFtvPLk0yJmO1LkCA'
+      'token': token
     }
   }).then(response => {
     setProfile(response.data)
@@ -45,9 +47,9 @@ export function getUserProfile(id, setProfile) {
 }
 
 export function unblockUser(id, setProfile) {
-  axios.post(GATEWAY_URL + "/users/unblock/" + id, {
+  axios.post('https://fiuber-gateway.herokuapp.com/admin/register/4ubbG910SyTA4iVCXDxdKmBwrPB2', {
     headers: {
-      'token': 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImQ3YjE5MTI0MGZjZmYzMDdkYzQ3NTg1OWEyYmUzNzgzZGMxYWY4OWYiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vZml1YmVyLTM2Yjg2IiwiYXVkIjoiZml1YmVyLTM2Yjg2IiwiYXV0aF90aW1lIjoxNjY4MDM0ODQzLCJ1c2VyX2lkIjoiZjdrMnF3dHc5NWhZcmF0bTZkNVZoc1VLTjY1MiIsInN1YiI6ImY3azJxd3R3OTVoWXJhdG02ZDVWaHNVS042NTIiLCJpYXQiOjE2NjgwMzQ4NDMsImV4cCI6MTY2ODAzODQ0MywiZW1haWwiOiJhZG1pbmNvbnRyYXNlbmlhc3RyaW5nQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJhZG1pbmNvbnRyYXNlbmlhc3RyaW5nQGdtYWlsLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIn19.On6dcaVudVMcGEOFWga8OHP5_kHIF6_-53NMSWC2ojeAFPrSyMQLiI-vTyD9UuxzhpY_NgA0x6WKtkUDmbHMH4G5NgqYys6GGE8-SaZkbR_8T4Gr_AJxoCDP8fWvzr4cmygo-S1dQSlSCmPB-rZJhWrVGNXQlip1DbGogK6hJS4KvmBeBd7OBo36H5I9wUCak7z6oWUJk79KD1JN6u8exdCNfJv5C0j5_ZLG6H6GBIXnwzU6B0E2ZoeAzBrxXpWoBPSz2iJwRpd52A99v-nvFV36iMPcUamHapxlmBxulo_5Tfni6ho5l9PAMrNnQ6qf0xRlByFtvPLk0yJmO1LkCA'
+      'token': token
     }
   }).then(response => {
     setProfile(response.data)
@@ -55,9 +57,9 @@ export function unblockUser(id, setProfile) {
 }
 
 export function blockUser(id, setProfile) {
-  axios.post(GATEWAY_URL + "/users/block/" + id, {
+  axios.post(GATEWAY_URL + "/admin/block/" + id, {
     headers: {
-      'token': 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImQ3YjE5MTI0MGZjZmYzMDdkYzQ3NTg1OWEyYmUzNzgzZGMxYWY4OWYiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vZml1YmVyLTM2Yjg2IiwiYXVkIjoiZml1YmVyLTM2Yjg2IiwiYXV0aF90aW1lIjoxNjY4MDM0ODQzLCJ1c2VyX2lkIjoiZjdrMnF3dHc5NWhZcmF0bTZkNVZoc1VLTjY1MiIsInN1YiI6ImY3azJxd3R3OTVoWXJhdG02ZDVWaHNVS042NTIiLCJpYXQiOjE2NjgwMzQ4NDMsImV4cCI6MTY2ODAzODQ0MywiZW1haWwiOiJhZG1pbmNvbnRyYXNlbmlhc3RyaW5nQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJhZG1pbmNvbnRyYXNlbmlhc3RyaW5nQGdtYWlsLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIn19.On6dcaVudVMcGEOFWga8OHP5_kHIF6_-53NMSWC2ojeAFPrSyMQLiI-vTyD9UuxzhpY_NgA0x6WKtkUDmbHMH4G5NgqYys6GGE8-SaZkbR_8T4Gr_AJxoCDP8fWvzr4cmygo-S1dQSlSCmPB-rZJhWrVGNXQlip1DbGogK6hJS4KvmBeBd7OBo36H5I9wUCak7z6oWUJk79KD1JN6u8exdCNfJv5C0j5_ZLG6H6GBIXnwzU6B0E2ZoeAzBrxXpWoBPSz2iJwRpd52A99v-nvFV36iMPcUamHapxlmBxulo_5Tfni6ho5l9PAMrNnQ6qf0xRlByFtvPLk0yJmO1LkCA'
+      'token': token
     }
   }).then(response => {
     setProfile(response.data)
