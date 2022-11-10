c4d5b3c8d82005f3de06321e5c386eef4ffb980c
niragui <ignacioiragu2@gmail.com>
2022-11-10 00:44:51 -0300

    Cierre Usuarios

--- src/api/Users/index.js
+++ src/api/Users/index.js
@@ -2,37 +2,16 @@ import axios from 'axios';
 
 const GATEWAY_URL = "https://fiuber-gateway.herokuapp.com"
 
-const token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImQ3YjE5MTI0MGZjZmYzMDdkYzQ3NTg1OWEyYmUzNzgzZGMxYWY4OWYiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vZml1YmVyLTM2Yjg2IiwiYXVkIjoiZml1YmVyLTM2Yjg2IiwiYXV0aF90aW1lIjoxNjY4MDQ3NTUzLCJ1c2VyX2lkIjoiZjdrMnF3dHc5NWhZcmF0bTZkNVZoc1VLTjY1MiIsInN1YiI6ImY3azJxd3R3OTVoWXJhdG02ZDVWaHNVS042NTIiLCJpYXQiOjE2NjgwNDc1NTMsImV4cCI6MTY2ODA1MTE1MywiZW1haWwiOiJhZG1pbmNvbnRyYXNlbmlhc3RyaW5nQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJhZG1pbmNvbnRyYXNlbmlhc3RyaW5nQGdtYWlsLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIn19.aPlR8sDqsy_h0jiCNxSFkrKY-w45uXBUWsIDvQtzTIStRKVilrQ5xeTOc9PDHUKuv-Fg6JA_WbYzoC5saqnSfKqXLJU0TO6BsSyeBNKodqmT0kshmuGZ3EDz9RYgGg8fGuVOvlv6HjaEGLC6WdYnYbOo9lBCeGM3xojSxemvAuIDvEdVVFos5oYIO3HzKnmFloKdg75a6m_CAVpdAxoNm0msGjV0CrPm254c4Nw2HB95FRrlrSFwtTtFy1SVQswL8xmwIJj0zSfJzWfaI23O24mMPD1zbUSVeiVCCpsAsyfW_YtPHyQvigvBGLddbJkMtyg0441ynmO9cxtoTNdP8w'
+const token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImQ3YjE5MTI0MGZjZmYzMDdkYzQ3NTg1OWEyYmUzNzgzZGMxYWY4OWYiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vZml1YmVyLTM2Yjg2IiwiYXVkIjoiZml1YmVyLTM2Yjg2IiwiYXV0aF90aW1lIjoxNjY4MDUxNzU3LCJ1c2VyX2lkIjoiZjdrMnF3dHc5NWhZcmF0bTZkNVZoc1VLTjY1MiIsInN1YiI6ImY3azJxd3R3OTVoWXJhdG02ZDVWaHNVS042NTIiLCJpYXQiOjE2NjgwNTE3NTcsImV4cCI6MTY2ODA1NTM1NywiZW1haWwiOiJhZG1pbmNvbnRyYXNlbmlhc3RyaW5nQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJhZG1pbmNvbnRyYXNlbmlhc3RyaW5nQGdtYWlsLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIn19.MNrZxWUTok3sBdfbc7Lj1zdn2kaljiKUIANJp9BOTvVNzeeqdnQvNLK5Je6Q7UFhtP53fuaAoTPahhHhGrke6Y1cuKpFboigNxHpNCWJwkAyFUupxhj_jQwXOs9p278rtvjb0RXv6gDJBSHxN2xRb84QfvgkllQ19tZ9xn4PIKiO33DzQIQwIkpttBWGIyA-PA3dL8WOtQGHRa4555Tszjtidz9iV5uD-4PbpSsmn1LUg59SHm082Ugy26iCCCjZ57I5-sO3AmgQbzTu-jZjWk-pUnoSzBGwL3Cvwm7_FYjV2LM6C6rdop6rWtrW1_1WvFiY7aoHB2bY1cKlgG1_WQ'
 
-export function getUsers() {
-  // Aca deberiamos hacer una llamada al gateway
-  // Cambiar el nombre de las columnas en modules/Users
-  // para que quede bien luego de la llamada.
-  
-  return [
-    {
-      key: 1,
-      id: '4ubbG910SyTA4iVCXDxdKmBwrPB2',
-      name: 'PASAJERO',
-      last_name: 'Elmas Capo',
-      roles: ['Passenger'],
-    },
-    {
-      key: 2,
-      id: 'cmyaQJfWuifQ4KFqBDyymgJRxOG2',
-      name: 'CHOFER + PASAJERO',
-      last_name: 'Facha',
-      roles: ['Passenger', 'Driver'],
-    },
-    {
-      key: 3,
-      id: 'f7k2qwtw95hYratm6d5VhsUKN652',
-      name: 'ADMIN',
-      last_name: 'Facha',
-      roles: ['Admin', 'Driver'],
-    },
-    
-  ];
+export function getUsers(setUsers) {
+  axios.get(GATEWAY_URL + "/admin/users", {
+    headers: {
+      'token': token
+    }
+  }).then(response => {
+    setUsers(response.data)
+  });
 }
 
 
--- src/modules/Users/index.js
+++ src/modules/Users/index.js
@@ -2,6 +2,7 @@ import { Card, Table, Tag } from "antd";
 import { getUsers } from "../../api/Users"
 import { useNavigate } from "react-router-dom";
 import { renderRoles } from "./Roles"
+import { useEffect, useState } from "react";
 
 const tableColumns = [
   {
@@ -31,7 +32,10 @@ const tableColumns = [
 
 const Users = () => {
   const navigate = useNavigate();
-  const users = getUsers();
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    getUsers(setUsers);
+  }, []);
 
   return (
     <Card title={'Usuarios'} style={{margin: 20}}>
