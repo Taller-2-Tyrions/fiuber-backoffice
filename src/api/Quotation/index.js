1e9ba6a2f6a4bd4c1bd99094e531d5ee1baa188e
niragui <ignacioiragu2@gmail.com>
2022-12-05 01:28:07 -0300

    Arreglos Pricing

--- src/api/Quotation/index.js
+++ src/api/Quotation/index.js
@@ -4,44 +4,35 @@ const GATEWAY_URL = "https://fiuber-gateway.herokuapp.com"
 
 const token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImY4MDljZmYxMTZlNWJhNzQwNzQ1YmZlZGE1OGUxNmU4MmYzZmQ4MDUiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vZml1YmVyLTM2Yjg2IiwiYXVkIjoiZml1YmVyLTM2Yjg2IiwiYXV0aF90aW1lIjoxNjY4NzE5MTQzLCJ1c2VyX2lkIjoiZjdrMnF3dHc5NWhZcmF0bTZkNVZoc1VLTjY1MiIsInN1YiI6ImY3azJxd3R3OTVoWXJhdG02ZDVWaHNVS042NTIiLCJpYXQiOjE2Njg3MTkxNDMsImV4cCI6MTY2ODcyMjc0MywiZW1haWwiOiJhZG1pbmNvbnRyYXNlbmlhc3RyaW5nQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJhZG1pbmNvbnRyYXNlbmlhc3RyaW5nQGdtYWlsLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIn19.GI31wavv-9qQqKZl7Rc3hzUzQHdPgb9j-i9nT55yJG1p_GpRn8elFPSvrxBo05VWp70TIPo8AciUZu6Dom9IJmGuIBl5p7rKE6qz1RbF9VQlyz-aEWd8eM6oy1L6Yg_S_8cHEQG8F6H2BoBbNmkK3Q2qxDMUQJlKV60jWlz9635Xt4dAe_CC01I55PtgggRJpJsii0HmKMLf60HN4LJ0s6ba16WQer2rGFlJzUwjFF3a2W5hkCo7AFqQJk7q1yUIAeyVjFl5nkJMhQ8BEC4W8bxxb2nLnDqjyC9v_CHfCDmr6fKJ29itWMSY9jcuDdCrNhrM0h88r3j3p7w2y16Rzg'
 
-export function getUsers(setUsers) {
-  axios.get(GATEWAY_URL + "/admin/users", {
-    headers: {
-      'token': token
-    }
-  }).then(response => {
-    setUsers(response.data)
-  });
+
+export function getConstants(setConstants) {
+    axios.get(GATEWAY_URL + "/admin/constants", {
+      headers: {
+        'token': token
+      }
+    }).then(response => {
+        setConstants(response.data)
+    });
 }
 
-
-export function getUserProfile(id, setProfile) {
-  axios.get(GATEWAY_URL + "/users/" + id, {
-    headers: {
-      'token': token
-    }
-  }).then(response => {
-    setProfile(response.data)
-  });
-}
-
-export function unblockUser(id, setProfile) {
-  axios.post(GATEWAY_URL + "/admin/unblock/" + id, {},  {
-    data: {},
-    headers: {
-      'token': token
-    }
-  }).then(response => {
-    getUserProfile(id, setProfile)
-  });
-}
-
-export function blockUser(id, setProfile) {
-  axios.post(GATEWAY_URL + "/admin/block/" + id, {}, {
-    headers: {
-      'token': token
-    }
-  }).then(response => {
-    getUserProfile(id, setProfile)
-  });
+export function sendConstants(constants, token) {
+    axios.put(GATEWAY_URL + "/admin/constants", {
+      headers: {
+        'token': token
+      },
+      json: {
+        "price_meter": constants.price_meter,
+        "price_minute": constants.price_minute,
+        "price_vip": constants.price_vip,
+        "plus_night": constants.plus_night,
+        "seniority_driver": constants.seniority_driver,
+        "daily_driver": constants.daily_driver,
+        "monthly_driver": constants.monthly_driver,
+        "seniority_passenger": constants.seniority_passenger,
+        "daily_passenger": constants.daily_passenger,
+        "monthly_passenger": constants.monthly_passenger,
+        "max_discount_passenger": constants.max_discount_passenger,
+        "max_increase_driver": constants.max_increase_driver
+      }
+    })
 }
--- src/modules/Quotation/index.js
+++ src/modules/Quotation/index.js
@@ -1,9 +1,10 @@
 import { createContext, useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { getUserProfile } from "../../api/Users"
+import { getConstants } from "../../api/Quotation"
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+
+const GATEWAY_URL="https://fiuber-gateway.herokuapp.com/admin/constants"
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -12,189 +13,164 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
-const ItemLeft = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  color: theme.palette.text.secondary,
-}));
-
-function renderSubmitButtons(){
-    return (
+function renderSubmitButton(){
+  return(
+    <div>
+      <Item>
         <div>
-            <Grid item md={6} xs={12}>
-                <Item>
-                    <div>
-                        <input type="submit" defaultValue="Submit" />
-                    </div>
-                </Item>
-            </Grid>
-                <Grid item md={6} xs={12}>
-                <Item>
-                    <div>
-                        <input type="reset" />
-                    </div>
-                </Item>
-            </Grid>
+        <input type="submit" defaultValue="Confirmar" style = {{fontSize:30, backgroundColor:"#2BF580"}}/>
         </div>
-    )
+      </Item>
+    </div>
+  )
+}
+
+function renderResetButton(){
+  return(
+    <div>
+      <Item>
+        <div>
+            <input type="reset"  defaultValue="Resetear" style = {{fontSize:30, backgroundColor:"#F75C5C"}}/>
+        </div>
+      </Item>
+    </div>
+  )
+}
+
+function renderDataButton(text, name, value, clarification, setFunction){
+  return(
+    <Item>
+      <div>
+          <label htmlFor={name} style = {{fontSize:20}}> <b>{text}</b></label>
+          <br/>
+            <input
+            type="number"
+            step="any"
+            min = "0"
+            id={name}
+            name={name}
+            defaultValue={value}
+            onChange={setFunction(prevState => {
+                let jasper = Object.assign({}, prevState.jasper);  // creating copy of state variable jasper
+                jasper.name = 'someothername';                     // update the name property, assign a new value                 
+                return { jasper };                                 // return new object jasper object
+              })}
+            />
+          <br />
+          {/* <input type="number" step="any" min = "0" id={name} name={name} defaultValue={value} /><br /> */}
+          <p>{clarification}</p>
+      </div>
+    </Item>
+  )
+}
+
+function sendSubmit(){
+    let i = 0
+}
+
+function quotationForm(constants, setFunction){
+    return (
+        <form onSubmit={sendSubmit()}>
+            <Grid container spacing = {1}>
+                <Grid item md={12} xs={12}>
+                <Item>
+                    <h1 style = {{fontSize:50}}>Cotización</h1>
+                </Item>
+                </Grid>
+    
+                <Grid item lg={4} md={6} xs={12}>
+                  {renderDataButton("Precio Por Minuto", "price_minute", constants.price_minute, "Precio por minuto esperado del viaje", setFunction)}
+                </Grid>
+    
+                <Grid item lg={4} md={6} xs={12}>
+                  {renderDataButton("Precio Por Metro", "price_meter", constants.price_meter, "Precio por metro esperado del viaje", setFunction)}
+                </Grid>
+    
+                <Grid item lg={4} md={6} xs={12}>
+                  {renderDataButton("Multiplicador VIP", "price_vip", constants.price_vip, "Mulplica el precio final por este valor si es un servicio tipo VIP", setFunction)}
+                </Grid>
+    
+                <Grid item lg={4} md={6} xs={12}>
+                  {renderDataButton("Multiplicador Nocturno", "plus_night", constants.plus_night, "Mulplica el precio final por este valor si es un servicio nocturno", setFunction)}
+                </Grid>
+    
+                <Grid item lg={4} md={6} xs={12}>
+                  {renderDataButton("Incremento Maximo Chofer", "max_increase_driver", constants.max_increase_driver, "Cantidad maxima de incremento de ganancia para el chofer", setFunction)}
+                </Grid>
+    
+                <Grid item lg={4} md={6} xs={12}>
+                  {renderDataButton("Descuento Maximo Pasajero", "max_discount_passenger", constants.max_discount_passenger, "Cantidad maxima de descuento para el chofer", setFunction)}
+                </Grid>
+    
+                <Grid item lg={4} md={6} xs={12}>
+                  {renderDataButton("Multiplicador Antigüedad Chofer", "seniority_driver", constants.seniority_driver, "Mulplica la cantidad de dias para generar un incremento de ganancia al chofer", setFunction)}
+                </Grid>
+    
+                <Grid item lg={4} md={6} xs={12}>
+                    {renderDataButton("Multiplicador Viajes Diarios Chofer", "daily_driver", constants.daily_driver, "Mulplica la cantidad de viajes diarios para generar un incremento de ganancia al chofer", setFunction)}
+                </Grid>
+    
+                <Grid item lg={4} md={6} xs={12}>
+                    {renderDataButton("Multiplicador Viajes Mensuales Chofer", "monthly_driver", constants.monthly_driver, "Mulplica la cantidad de viajes mensuales para generar un incremento de ganancia al chofer", setFunction)}
+                </Grid>
+    
+                <Grid item lg={4} md={6} xs={12}>
+                  {renderDataButton("Multiplicador Antigüedad Pasajero", "seniority_passenger", constants.seniority_passenger, "Mulplica la cantidad de dias para generar un incremento de ganancia al pasajero", setFunction)}
+                </Grid>
+    
+                <Grid item lg={4} md={6} xs={12}>
+                    {renderDataButton("Multiplicador Viajes Diarios Pasajero", "daily_passenger", constants.daily_passenger, "Mulplica la cantidad de viajes diarios para generar un incremento de ganancia al pasajero", setFunction)}
+                </Grid>
+    
+                <Grid item lg={4} md={6} xs={12}>
+                    {renderDataButton("Multiplicador Viajes Mensuales Pasajero", "monthly_passenger", constants.monthly_passenger, "Mulplica la cantidad de viajes mensuales para generar un incremento de ganancia al pasajero", setFunction)}
+                </Grid>
+    
+                <Grid item xs={12}>
+                <Item>
+                   <div>
+                        <p style = {{fontSize:30}}><b>Por Favor Asegurese Los Valores Cargados Son Correctos Antes De Confirmar</b></p>
+                    </div>
+                </Item>
+                </Grid>
+    
+                <Grid item md={6} xs={12}>
+                    {renderSubmitButton()}
+                </Grid>
+                <Grid item md={6} xs={12}>
+                    {renderResetButton()}
+                </Grid>
+            </Grid>
+        </form>
+      );
 }
 
 const Quotation = () => {
-  return (
-    <form>
-        <Grid container spacing = {1}>
-            <Grid item md={12} xs={12}>
-            <Item>
-                <h1 style = {{fontSize:50}}>Cotización</h1>
-            </Item>
-            </Grid>
+    const [constants, setConstants] = useState({
+        "price_meter": 1.0,
+        "price_minute": 2.0,
+        "price_vip": 1.0,
+        "plus_night": 1.0,
+        "seniority_driver": 1.0,
+        "daily_driver": 1.0,
+        "monthly_driver": 1.0,
+        "seniority_passenger": 1.0,
+        "daily_passenger": 1.0,
+        "monthly_passenger": 1.0,
+        "max_discount_passenger": 1.0,
+        "max_increase_driver": 1.0
+      });
 
-            <Grid item lg={4} md={6} xs={12}>
-            <Item>
-               <div>
-                    <label htmlFor="fname" style = {{fontSize:20}}> <b>Precio Por Minuto:</b></label><br/>
-                    <input type="number" step="any" min = "0" id="fname" name="fname" defaultValue="3" /><br />
-                    <p>Precio por minuto esperado del viaje</p>
-                </div>
-            </Item>
-            </Grid>
+    useEffect(() => {
+        getConstants(setConstants);
+    }, []);
 
-            <Grid item lg={4} md={6} xs={12}>
-            <Item>
-               <div>
-                    <label htmlFor="fname" style = {{fontSize:20}}> <b>Precio Por Kilometro:</b></label><br/>
-                    <input type="number" step="any" min = "0" id="fname" name="fname" defaultValue="3" /><br />
-                    <p>Precio por kilometro del trayecto a realizar</p>
-                </div>
-            </Item>
-            </Grid>
-
-            <Grid item lg={4} md={6} xs={12}>
-            <Item>
-               <div>
-                    <label htmlFor="fname" style = {{fontSize:20}}> <b>Multiplicador VIP:</b></label><br/>
-                    <input type="number" step="any" min = "1" id="fname" name="fname" defaultValue="3" /><br />
-                    <p>Mulplica el precio final por este valor si es un servicio tipo VIP</p>
-                </div>
-            </Item>
-            </Grid>
-
-            <Grid item lg={4} md={6} xs={12}>
-            <Item>
-               <div>
-                    <label htmlFor="fname" style = {{fontSize:20}}> <b>Multiplicador Nocturno:</b></label><br/>
-                    <input type="number" step="any" min = "1" id="fname" name="fname" defaultValue="3" /><br />
-                    <p>Mulplica el precio final por este valor si es un servicio nocturno</p>
-                </div>
-            </Item>
-            </Grid>
-
-            <Grid item lg={4} md={6} xs={12}>
-            <Item>
-               <div>
-                    <label htmlFor="fname" style = {{fontSize:20}}> <b>Incremento Maximo Chofer:</b></label><br/>
-                    <input type="number" step="any" min = "0" id="fname" name="fname" defaultValue="3" /><br />
-                    <p>Cantidad maxima de incremento de ganancia para el chofer</p>
-                </div>
-            </Item>
-            </Grid>
-
-            <Grid item lg={4} md={6} xs={12}>
-            <Item>
-               <div>
-                    <label htmlFor="fname" style = {{fontSize:20}}> <b>Descuento Maximo Pasajero:</b></label><br/>
-                    <input type="number" step="any" min = "0" id="fname" name="fname" defaultValue="3" /><br />
-                    <p>Cantidad maxima de descuento para el chofer</p>
-                </div>
-            </Item>
-            </Grid>
-
-            <Grid item lg={4} md={6} xs={12}>
-            <Item>
-               <div>
-                    <label htmlFor="fname" style = {{fontSize:20}}> <b>Multiplicador Antigüedad Chofer:</b></label><br/>
-                    <input type="number" step="any" min = "0" id="fname" name="fname" defaultValue="3" /><br />
-                    <p>Mulplica la cantidad de dias para generar un incremento de ganancia al chofer</p>
-                </div>
-            </Item>
-            </Grid>
-
-            <Grid item lg={4} md={6} xs={12}>
-            <Item>
-               <div>
-                    <label htmlFor="fname" style = {{fontSize:20}}> <b>Multiplicador Viajes Diarios Chofer:</b></label><br/>
-                    <input type="number" step="any" min = "0" id="fname" name="fname" defaultValue="3" /><br />
-                    <p>Mulplica la cantidad de viajes diarios para generar un incremento de ganancia al chofer</p>
-                </div>
-            </Item>
-            </Grid>
-
-            <Grid item lg={4} md={6} xs={12}>
-            <Item>
-               <div>
-                    <label htmlFor="fname" style = {{fontSize:20}}> <b>Multiplicador Viajes Mensuales Chofer:</b></label><br/>
-                    <input type="number" step="any" min = "0" id="fname" name="fname" defaultValue="3" /><br />
-                    <p>Mulplica la cantidad de viajes mensuales para generar un incremento de ganancia al chofer</p>
-                </div>
-            </Item>
-            </Grid>
-
-            <Grid item lg={4} md={6} xs={12}>
-            <Item>
-               <div>
-                    <label htmlFor="fname" style = {{fontSize:20}}> <b>Multiplicador Antigüedad Pasajero:</b></label><br/>
-                    <input type="number" step="any" min = "0" id="fname" name="fname" defaultValue="3" /><br />
-                    <p>Mulplica la cantidad de dias para generar un descuento al Pasajero</p>
-                </div>
-            </Item>
-            </Grid>
-
-            <Grid item lg={4} md={6} xs={12}>
-            <Item>
-               <div>
-                    <label htmlFor="fname" style = {{fontSize:20}}> <b>Multiplicador Viajes Diarios Pasajero:</b></label><br/>
-                    <input type="number" step="any" min = "0" id="fname" name="fname" defaultValue="3" /><br />
-                    <p>Mulplica la cantidad de viajes diarios para generar un descuento al Pasajero</p>
-                </div>
-            </Item>
-            </Grid>
-
-            <Grid item lg={4} md={6} xs={12}>
-            <Item>
-               <div>
-                    <label htmlFor="fname" style = {{fontSize:20}}> <b>Multiplicador Viajes Mensuales Pasajero:</b></label><br/>
-                    <input type="number" step="any" min = "0" id="fname" name="fname" defaultValue="3" /><br />
-                    <p>Mulplica la cantidad de viajes mensuales para generar un descuento al Pasajero</p>
-                </div>
-            </Item>
-            </Grid>
-
-            <Grid item xs={12}>
-            <Item>
-               <div>
-                    <p style = {{fontSize:30}}><b>Por Favor Asegurese Los Valores Cargados Son Correctos Antes De Confirmar</b></p>
-                </div>
-            </Item>
-            </Grid>
-
-            <Grid item md={6} xs={12}>
-                <Item>
-                    <div>
-                        <input type="submit" defaultValue="Confirmar" style = {{fontSize:30, backgroundColor:"#2BF580"}}/>
-                    </div>
-                </Item>
-            </Grid>
-                <Grid item md={6} xs={12}>
-                <Item>
-                    <div>
-                        <input type="reset"  defaultValue="Resetear" style = {{fontSize:30, backgroundColor:"#F75C5C"}}/>
-                    </div>
-                </Item>
-            </Grid>
-        </Grid>
-    </form>
-  );
+    return(
+        <div>
+            {quotationForm(constants)}
+        </div>
+    )
+    
 }
 
 export default Quotation;
