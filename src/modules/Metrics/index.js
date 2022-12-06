3f72295a04c4ccaac7890b494b2b7857a881a5dc
mateocapon <mcapon@fi.uba.ar>
2022-12-05 21:21:44 -0300

    empezando graficos

--- src/modules/Metrics/index.js
+++ src/modules/Metrics/index.js
@@ -1,10 +1,58 @@
+import { Chart } from 'primereact/chart';
+import { useState } from 'react';
 
-const Transactions = () => {
+
+const Metrics = () => {
+  let horizontalOptions = {
+    indexAxis: 'y',
+    maintainAspectRatio: false,
+    aspectRatio: .8,
+    plugins: {
+        legend: {
+            labels: {
+                color: '#495057'
+            }
+        }
+    },
+    scales: {
+        x: {
+            ticks: {
+                color: '#495057'
+            },
+            grid: {
+                color: '#ebedef'
+            }
+        },
+        y: {
+            ticks: {
+                color: '#495057'
+            },
+            grid: {
+                color: '#ebedef'
+            }
+        }
+    }
+  };
+
+  const [basicData] = useState({
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+        {
+            label: 'My First dataset',
+            backgroundColor: '#42A5F5',
+            data: [65, 59, 80, 81, 56, 55, 40]
+        }
+    ]
+  });
+
+
+
     return (
-      <div>
-        Falta implementar
+      <div className="card">
+        <h5>Horizontal</h5>
+        <Chart type="bar" data={basicData} options={horizontalOptions} />
       </div>
     );
   };
   
-  export default Transactions;
+  export default Metrics;
